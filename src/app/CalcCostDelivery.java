4a601a980b3e784c604fda7fccce47593980c71f
MaxKompa <max.kompa645@gmail.com>
2024-06-19 19:54:39 +0200

    Added inheritance from the CalcCostBase to CalcCostDelivery

--- src/app/CalcCostDelivery.java
+++ src/app/CalcCostDelivery.java
@@ -4,13 +4,14 @@ package app;
 // враховуючи вартість доставки
 // ЗАВДАННЯ: Виправити код класу.
 // Клас успадковує клас CalcCostBase.
-public class CalcCostDelivery {
+public class CalcCostDelivery extends CalcCostBase{
 
     // Вартість доставки
     private final static double deliveryPrice = 7;
 
     // Розрахунок вартості товару,
     // з урахуванням вартості доставки
+    @Override
     public double calcCost(Product product) {
         return product.getQuota() * product.getPrice()
                 + deliveryPrice;
